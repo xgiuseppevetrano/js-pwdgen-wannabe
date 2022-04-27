1a6e5a4ac2b7a7ea389d294edb6fe007faa5a889
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-27 17:07:27 +0200

    add fourth const

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Chiedo all’utente il suo nome e lo salvo
-const name = prompt ("Qual è il tuo nome?");
+const firstName = prompt ("Qual è il tuo nome?");
 
 // Chiedo il suo cognome e lo salvo
-const surname = prompt ("Qual è il tuo cognome?");
+const secondName = prompt ("Qual è il tuo cognome?");
 
 // Chiedo il suo colore preferito e lo salvo
 const color = prompt ("Qual è il tuo colore preferito?");
 
 // Stampo in console nomecognomecolorepreferito21
-
-
+const password = firstName + secondName + color + 21;
+console.log(password);
 // Scrivo nell'html la password generata
