b40a964527bd7ba4869e6b85836cbab9b7c23cc3
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-27 16:55:36 +0200

    add second const

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const name = prompt ("Qual è il tuo nome?");
 
 // Chiedo il suo cognome e lo salvo
-
+const surname = prompt ("Qual è il tuo cognome?");
 
 // Chiedo il suo colore preferito e lo salvo
 
