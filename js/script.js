2311b13e92eb097d68c1fb1f432db12858d18b00
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-27 16:43:52 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiedi all’utente il suo nome e lo salvo
+
+// Chiedo il suo cognome e lo salvo
+
+// Chiedo il suo colore preferito e lo salvo
+
+// Stampo in console nomecognomecolorepreferito21
+
+// Scrivo nell'html la password generata
