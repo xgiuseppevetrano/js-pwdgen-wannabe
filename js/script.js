f0e5f5a47e3db55f5ada6c17aee310da2b261459
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-27 16:50:15 +0200

    add first const

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-// Chiedi all’utente il suo nome e lo salvo
+// Chiedo all’utente il suo nome e lo salvo
+const name = prompt ("Qual è il tuo nome?");
 
 // Chiedo il suo cognome e lo salvo
 
+
 // Chiedo il suo colore preferito e lo salvo
 
+
 // Stampo in console nomecognomecolorepreferito21
+
 
 // Scrivo nell'html la password generata
